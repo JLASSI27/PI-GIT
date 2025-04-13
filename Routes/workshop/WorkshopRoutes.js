296be9159d03017b4a3d476f1d635c9e74df9796
MOUMNIMEHER <156126895+MOUMNIMEHER@users.noreply.github.com>
2025-04-13 15:55:43 +0100

    added is admin to workshop

--- Routes/workshop/WorkshopRoutes.js
+++ Routes/workshop/WorkshopRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const workshopController = require('../../Controllers/Workshop/workshopController');
 const { validateWorkshop } = require('../../Middlewares/workshop/validateWorkshop');
-const upload = require('../../Middlewares/workshop/upload'); // Import du middleware d'upload
+const upload = require('../../Middlewares/workshop/upload');
+const isAdmin = require("../../Middlewares/User/isAdmin.middleware"); // Import du middleware d'upload
 
 //  Création d'un workshop avec upload + validation
-router.post('/', upload.single('image'), validateWorkshop, workshopController.createWorkshop);
+router.post('/', upload.single('image'), validateWorkshop,isAdmin, workshopController.createWorkshop);
 
 //  Modification d'un workshop avec option de mise à jour d'image
-router.put('/:id', upload.single('image'), validateWorkshop, workshopController.updateWorkshop);
+router.put('/:id', upload.single('image'), validateWorkshop,isAdmin, workshopController.updateWorkshop);
 
 // Recommandations
 router.get('/recommend', workshopController.recommendWorkshops);
@@ -16,7 +17,7 @@ router.get('/recommend', workshopController.recommendWorkshops);
 // Autres opérations
 router.get('/', workshopController.getWorkshops);
 router.get('/:id', workshopController.getWorkshopById);
-router.delete('/:id', workshopController.deleteWorkshop);
+router.delete('/:id',isAdmin, workshopController.deleteWorkshop);
 
 // Moyenne des avis
 router.get('/:workshopId/average-rating', workshopController.getWorkshopAverageRating);
